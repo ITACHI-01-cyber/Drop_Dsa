a4f9e049b96e24abf4667fede843c8cd05f05712
ITACHI-01-cyber <bvivek514@gmail.com>
2026-06-11 09:48:03 +0530

    queue day 01

--- LINKED LIST/CpQues.java
+++ LINKED LIST/CpQues.java
@@ -1,147 +1,288 @@
-
-// class Node{
-//     int data;
-//     Node next;
-//     Node(int data){
-//         this.data = data;
-//         this.next = null;
-//     }
-// }
-
-// public class CpQues {
-//     Node head = null;
-//     void add(int data){
-//         Node newNode = new Node(data);
-//         if (head == null) {
-//             head = newNode;
-//             return;
-//         }
-//         Node temp = head;
-//         while(temp.next != null){
-//             temp = temp.next;
-//         }
-//         temp.next = newNode;
-//     }
-//     void Print(){
-//         Node curr = head;
-//         while (curr != null) {
-//             System.out.print(curr.data+" -> ");
-//             curr = curr.next;
-//         }
-//         System.out.println("Null");
-//     }
-
-//     Node FindMiddle() {
-//         Node slow = head;
-//         Node fast = head;
-
-//         while (fast != null && fast.next != null) {
-//             slow = slow.next;
-//             fast = fast.next.next;
-//         }
-//         return slow;
-//     }
-
-//     void main() {
-//         CpQues list = new CpQues();
-//         list.add(10);
-//         list.add(20);
-//         list.add(30);
-//         list.add(40);
-//         list.add(50);
-//         list.Print();
-        
-//         Node mid = list.FindMiddle();
-//         if (mid != null) {
-//             System.out.println("The middle element is: " + mid.data);
-//         }
-//     }
-// }
-
-
-// MERGING TWO SORTED LINKED list ======================================================================================================================
-    
-//class Node {
-//    int data;
-//    Node next;
-//    Node(int x) { data = x; next = null; }
-//}
 //
-//public class CpQues {
+//// class Node{
+////     int data;
+////     Node next;
+////     Node(int data){
+////         this.data = data;
+////         this.next = null;
+////     }
+//// }
 //
-//    static Node mergeSort(Node head) {
-//        if (head == null || head.next == null)
-//            return head;
+//// public class CpQues {
+////     Node head = null;
+////     void add(int data){
+////         Node newNode = new Node(data);
+////         if (head == null) {
+////             head = newNode;
+////             return;
+////         }
+////         Node temp = head;
+////         while(temp.next != null){
+////             temp = temp.next;
+////         }
+////         temp.next = newNode;
+////     }
+////     void Print(){
+////         Node curr = head;
+////         while (curr != null) {
+////             System.out.print(curr.data+" -> ");
+////             curr = curr.next;
+////         }
+////         System.out.println("Null");
+////     }
 //
-//        Node mid = getMiddle(head);
-//        Node nextOfMid = mid.next;
-//        mid.next = null;
+////     Node FindMiddle() {
+////         Node slow = head;
+////         Node fast = head;
 //
-//        Node left = mergeSort(head);
-//        Node right = mergeSort(nextOfMid);
+////         while (fast != null && fast.next != null) {
+////             slow = slow.next;
+////             fast = fast.next.next;
+////         }
+////         return slow;
+////     }
 //
-//        return merge(left, right);
-//    }
+////     void main() {
+////         CpQues list = new CpQues();
+////         list.add(10);
+////         list.add(20);
+////         list.add(30);
+////         list.add(40);
+////         list.add(50);
+////         list.Print();
 //
-//    static Node getMiddle(Node head) {
-//        Node slow = head, fast = head.next;
-//        while (fast != null && fast.next != null) {
-//            slow = slow.next;
-//            fast = fast.next.next;
-//        }
-//        return slow;
-//    }
+////         Node mid = list.FindMiddle();
+////         if (mid != null) {
+////             System.out.println("The middle element is: " + mid.data);
+////         }
+////     }
+//// }
 //
-//    static Node merge(Node head1, Node head2) {
-//        Node dummy = new Node(-1);
-//        Node curr = dummy;
 //
-//        while (head1 != null && head2 != null) {
-//            if (head1.data <= head2.data) {
-//                curr.next = head1;
-//                head1 = head1.next;
-//            } else {
-//                curr.next = head2;
-//                head2 = head2.next;
-//            }
-//            curr = curr.next;
-//        }
+//// MERGING TWO SORTED LINKED list ======================================================================================================================
 //
-//        curr.next = (head1 != null) ? head1 : head2;
-//        return dummy.next;
-//    }
+////class Node {
+////    int data;
+////    Node next;
+////    Node(int x) { data = x; next = null; }
+////}
+////
+////public class CpQues {
+////
+////    static Node mergeSort(Node head) {
+////        if (head == null || head.next == null)
+////            return head;
+////
+////        Node mid = getMiddle(head);
+////        Node nextOfMid = mid.next;
+////        mid.next = null;
+////
+////        Node left = mergeSort(head);
+////        Node right = mergeSort(nextOfMid);
+////
+////        return merge(left, right);
+////    }
+////
+////    static Node getMiddle(Node head) {
+////        Node slow = head, fast = head.next;
+////        while (fast != null && fast.next != null) {
+////            slow = slow.next;
+////            fast = fast.next.next;
+////        }
+////        return slow;
+////    }
+////
+////    static Node merge(Node head1, Node head2) {
+////        Node dummy = new Node(-1);
+////        Node curr = dummy;
+////
+////        while (head1 != null && head2 != null) {
+////            if (head1.data <= head2.data) {
+////                curr.next = head1;
+////                head1 = head1.next;
+////            } else {
+////                curr.next = head2;
+////                head2 = head2.next;
+////            }
+////            curr = curr.next;
+////        }
+////
+////        curr.next = (head1 != null) ? head1 : head2;
+////        return dummy.next;
+////    }
+////
+////    static Node sortAndMerge(Node head1, Node head2) {
+////        head1 = mergeSort(head1);   // Sort list 1
+////        head2 = mergeSort(head2);   // Sort list 2
+////        return merge(head1, head2); // Merge both
+////    }
+////
+////    static void printList(Node head) {
+////        while (head != null) {
+////            System.out.print(head.data + (head.next != null ? " -> " : "\n"));
+////            head = head.next;
+////        }
+////    }
+////
+////        void main(String[] args) {
+////        Node head1 = new Node(15);
+////        head1.next = new Node(3);
+////        head1.next.next = new Node(40);
+////        head1.next.next.next = new Node(5);
+////
+////        Node head2 = new Node(20);
+////        head2.next = new Node(2);
+////        head2.next.next = new Node(10);
+////
+////        Node result = sortAndMerge(head1, head2);
+////        printList(result);
+////    }
+////}
 //
-//    static Node sortAndMerge(Node head1, Node head2) {
-//        head1 = mergeSort(head1);   // Sort list 1
-//        head2 = mergeSort(head2);   // Sort list 2
-//        return merge(head1, head2); // Merge both
-//    }
 //
-//    static void printList(Node head) {
-//        while (head != null) {
-//            System.out.print(head.data + (head.next != null ? " -> " : "\n"));
-//            head = head.next;
-//        }
-//    }
+//// REVERSE A LINKED LIST +==================================================>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 //
-//        void main(String[] args) {
-//        Node head1 = new Node(15);
-//        head1.next = new Node(3);
-//        head1.next.next = new Node(40);
-//        head1.next.next.next = new Node(5);
+////class Node{
+////    int data;
+////    Node next;
+////    Node(int data){
+////        this.data = data;
+////        this.next = null;
+////    }
+////}
+////
+////public class CpQues {
+////    Node head = null;
+////
+////    void add(int data) {
+////        Node newNode = new Node(data);
+////
+////        if (head == null) {
+////            head = newNode;
+////            return;
+////        }
+////        Node temp = head;
+////        while (temp.next != null) {
+////            temp = temp.next;
+////        }
+////        temp.next = newNode;
+////    }
+////
+////    void Print() {
+////        Node curr = head;
+////        while (curr != null) {
+////            System.out.print(curr.data + " -> ");
+////            curr = curr.next;
+////        }
+////        System.out.print("NULL");
+////    }
+////
+////    Node reverseList(Node head){
+////        Node prev = null;
+////        Node curr = head;
+////        while (curr != null){
+////            Node next = curr.next;
+////            curr.next = prev;
+////            prev = curr;
+////            curr = next;
+////        }
+////        return prev;
+////    }
+////
+////    void main(){
+////        CpQues list = new CpQues();
+////        list.add(10);
+////        list.add(20);
+////        list.add(30);
+////        list.add(40);
+////
+////        System.out.println("Original List:");
+////        list.Print();
+////        System.out.println();
+////
+////        list.head = list.reverseList(list.head);
+////
+////        System.out.println("Reversed List:");
+////        list.Print();
+////    }
+////}
 //
-//        Node head2 = new Node(20);
-//        head2.next = new Node(2);
-//        head2.next.next = new Node(10);
 //
-//        Node result = sortAndMerge(head1, head2);
-//        printList(result);
-//    }
-//}
-
-
-// REVERSE A LINKED LIST +==================================================>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
+//// REMOVE NTH NODE FROM A LINKED LIST
+//
+//// class Node{
+////    int data;
+////    Node next;
+////    Node(int data){
+////        this.data = data;
+////        this.next = null;
+////    }
+////}
+////
+////public class CpQues {
+////    Node head = null;
+////
+////    void add(int data) {
+////        Node newNode = new Node(data);
+////
+////        if (head == null) {
+////            head = newNode;
+////            return;
+////        }
+////        Node temp = head;
+////        while (temp.next != null) {
+////            temp = temp.next;
+////        }
+////        temp.next = newNode;
+////    }
+////
+////    void Print() {
+////        Node curr = head;
+////        while (curr != null) {
+////            System.out.print(curr.data + " -> ");
+////            curr = curr.next;
+////        }
+////        System.out.print("NULL");
+////    }
+////
+////    public Node removeNthFromEnd(Node head, int n) {
+////        Node dummy = new Node(0);
+////        dummy.next = head;
+////
+////        Node fast = dummy;
+////        Node slow = dummy;
+////
+////        for (int i = 0; i <= n; i++) {
+////            fast = fast.next;
+////        }
+////
+////        while (fast != null) {
+////            fast = fast.next;
+////            slow = slow.next;
+////        }
+////        slow.next = slow.next.next;
+////        return dummy.next;
+////    }
+////
+////    void main(){
+////        CpQues list = new CpQues();
+////        list.add(10);
+////        list.add(20);
+////        list.add(30);
+////        list.add(40);
+////
+////        System.out.println("Original List:");
+////        list.Print();
+////
+////        System.out.println(" ");
+////        list.removeNthFromEnd(list.head,2);
+////        list.Print();
+////
+////    }
+////}
+//
+////  REODER LINKED LIST =============================================================================================
+//
 //class Node{
 //    int data;
 //    Node next;
@@ -177,91 +318,44 @@
 //        System.out.print("NULL");
 //    }
 //
-//    Node reverseList(Node head){
-//        Node prev = null;
-//        Node curr = head;
-//        while (curr != null){
-//            Node next = curr.next;
-//            curr.next = prev;
-//            prev = curr;
-//            curr = next;
-//        }
-//        return prev;
-//    }
-//
-//    void main(){
-//        CpQues list = new CpQues();
-//        list.add(10);
-//        list.add(20);
-//        list.add(30);
-//        list.add(40);
-//
-//        System.out.println("Original List:");
-//        list.Print();
-//        System.out.println();
-//
-//        list.head = list.reverseList(list.head);
-//
-//        System.out.println("Reversed List:");
-//        list.Print();
-//    }
-//}
-
-
-// REMOVE NTH NODE FROM A LINKED LIST
-
-// class Node{
-//    int data;
-//    Node next;
-//    Node(int data){
-//        this.data = data;
-//        this.next = null;
-//    }
-//}
-//
-//public class CpQues {
-//    Node head = null;
-//
-//    void add(int data) {
-//        Node newNode = new Node(data);
-//
-//        if (head == null) {
-//            head = newNode;
+//    public void ReoderList(Node head){
+//        if (head == null || head.next == null){
 //            return;
 //        }
-//        Node temp = head;
-//        while (temp.next != null) {
-//            temp = temp.next;
-//        }
-//        temp.next = newNode;
-//    }
+//        // 1. Find the middle
+//        Node slow = head;
+//        Node fast = head;
 //
-//    void Print() {
-//        Node curr = head;
-//        while (curr != null) {
-//            System.out.print(curr.data + " -> ");
-//            curr = curr.next;
-//        }
-//        System.out.print("NULL");
-//    }
-//
-//    public Node removeNthFromEnd(Node head, int n) {
-//        Node dummy = new Node(0);
-//        dummy.next = head;
-//
-//        Node fast = dummy;
-//        Node slow = dummy;
-//
-//        for (int i = 0; i <= n; i++) {
-//            fast = fast.next;
-//        }
-//
-//        while (fast != null) {
-//            fast = fast.next;
+//        while (fast.next != null && fast.next.next != null) {
 //            slow = slow.next;
+//            fast = fast.next.next;
 //        }
-//        slow.next = slow.next.next;
-//        return dummy.next;
+//
+//        // 2. Reverse the second half
+//        Node second = slow.next;
+//        slow.next = null;
+//
+//        Node prev = null;
+//        while (second != null) {
+//            Node next = second.next;
+//            second.next = prev;
+//            prev = second;
+//            second = next;
+//        }
+//
+//        // 3. Merge first half and reversed second half
+//        Node first = head;
+//        second = prev;
+//        while (second != null) {
+//            Node temp1 = first.next;
+//            Node temp2 = second.next;
+//
+//            first.next = second;
+//            second.next = temp1;
+//
+//            first = temp1;
+//            second = temp2;
+//        }
 //    }
 //
 //    void main(){
@@ -275,102 +369,8 @@
 //        list.Print();
 //
 //        System.out.println(" ");
-//        list.removeNthFromEnd(list.head,2);
+//        list.ReoderList(list.head);
 //        list.Print();
 //
 //    }
 //}
-
-//  REODER LINKED LIST =============================================================================================
-
-class Node{
-    int data;
-    Node next;
-    Node(int data){
-        this.data = data;
-        this.next = null;
-    }
-}
-
-public class CpQues {
-    Node head = null;
-
-    void add(int data) {
-        Node newNode = new Node(data);
-
-        if (head == null) {
-            head = newNode;
-            return;
-        }
-        Node temp = head;
-        while (temp.next != null) {
-            temp = temp.next;
-        }
-        temp.next = newNode;
-    }
-
-    void Print() {
-        Node curr = head;
-        while (curr != null) {
-            System.out.print(curr.data + " -> ");
-            curr = curr.next;
-        }
-        System.out.print("NULL");
-    }
-
-    public void ReoderList(Node head){
-        if (head == null || head.next == null){
-            return;
-        }
-        // 1. Find the middle
-        Node slow = head;
-        Node fast = head;
-
-        while (fast.next != null && fast.next.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        // 2. Reverse the second half
-        Node second = slow.next;
-        slow.next = null;
-
-        Node prev = null;
-        while (second != null) {
-            Node next = second.next;
-            second.next = prev;
-            prev = second;
-            second = next;
-        }
-
-        // 3. Merge first half and reversed second half
-        Node first = head;
-        second = prev;
-        while (second != null) {
-            Node temp1 = first.next;
-            Node temp2 = second.next;
-
-            first.next = second;
-            second.next = temp1;
-
-            first = temp1;
-            second = temp2;
-        }
-    }
-
-    void main(){
-        CpQues list = new CpQues();
-        list.add(10);
-        list.add(20);
-        list.add(30);
-        list.add(40);
-
-        System.out.println("Original List:");
-        list.Print();
-
-        System.out.println(" ");
-        list.ReoderList(list.head);
-        list.Print();
-
-    }
-}
--- Queue/Q01.java
+++ Queue/Q01.java
@@ -1,3 +1,110 @@
-public class Q01 {
-    
+import java.util.*;
+
+
+// QUEUE implementation using JCF ++++++++++++++++++++++++++++++++++++++
+//public class Q01 {
+//    public static void main(String[] args) {
+//        Queue<Integer> que = new LinkedList<>();
+//        que.add(10);
+//        que.add(20);
+//        que.add(30);
+//
+//        System.out.println("Front :"+ que.peek() );
+//        System.out.println("Removed :"+ que.poll());
+//        System.out.println("Is Empty? :"+ que.isEmpty());
+//        System.out.println("Queue :" + que);
+//
+//    }
+//}
+
+// QUEUE implementation using Array ++++++++++++++++++++++++++++++++++++++
+// QUEUE implementation using Array
+class QueueArray {
+    private int[] arr;
+    private int front, rear, capacity, count;
+
+    QueueArray(int size) {
+        arr = new int[size];
+        capacity = size;
+        front = 0;
+        rear = -1;
+        count = 0;
+    }
+
+    // Enqueue
+    public void enqueue(int x) {
+        if (count == capacity) {
+            System.out.println("Queue Overflow");
+            return;
+        }
+        rear = (rear + 1) % capacity;
+        arr[rear] = x;
+        count++;
+    }
+
+    // Dequeue
+    public int dequeue() {
+        if (count == 0) {
+            System.out.println("Queue Underflow");
+            return -1;
+        }
+        int x = arr[front];
+        front = (front + 1) % capacity;
+        count--;
+        return x;
+    }
+
+    // Peek (front element)
+    public int peek() {
+        if (count == 0) {
+            System.out.println("Queue is empty");
+            return -1;
+        }
+        return arr[front];
+    }
+
+    // Get rear element (last)
+    public int getRear() {
+        if (count == 0) {
+            System.out.println("Queue is empty");
+            return -1;
+        }
+        return arr[rear];
+    }
+
+    public boolean isEmpty() {
+        return count == 0;
+    }
+    public void printQueue() {
+        if (count == 0) {
+            System.out.println("Queue is empty");
+            return;
+        }
+        System.out.print("Queue elements: ");
+        for (int i = 0; i < count; i++) {
+            int index = (front + i) % capacity;
+            System.out.print(arr[index] + " ");
+        }
+        System.out.println();
+    }
 }
+
+public class Q01 {
+    public static void main(String[] args) {
+        QueueArray q = new QueueArray(5);
+        q.enqueue(10);
+        q.enqueue(20);
+        q.enqueue(30);
+
+        q.printQueue();
+
+        q.dequeue();
+        q.printQueue();
+
+        q.enqueue(40);
+        q.enqueue(50);
+        q.enqueue(60);
+        q.printQueue();
+    }
+}
+
